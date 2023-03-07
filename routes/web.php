d5a827fddbb4b667f64c52773d0617c4a0d72bd3
Cruzdelangelrubiofierro <125680938+Cruzdelangelrubiofierro@users.noreply.github.com>
2023-03-07 07:19:04 -0700

    Boscore casi completo

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/Boxscore', function () {
+    return view('Boxscore');
 });
+Route::get('/B', function () {
+    return view('B');
+});
+    ?>
