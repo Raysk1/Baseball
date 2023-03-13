c0b22900253b91ae08dfe0bdd24d71fd7e0c00a4
Raysk1 <francogarciaoscar@gmail.com>
2023-03-13 08:06:02 -0700

    ampayer y juego controladores creados

--- app/Http/Controllers/AmpayerControlador.php
+++ app/Http/Controllers/AmpayerControlador.php
@@ -6,7 +6,34 @@ use Illuminate\Http\Request;
 
 class AmpayerControlador extends Controller
 {
-    public function create(Request $request){
+
+    /** 
+     * Display a listing of the resource. 
+     * @return \Illuminate\Http\Response 
+     */
+    public function index() {
+
+        $datos = Ampayer::all();
+        return response(view("Ampayers.index", compact("datos")));
+    }
+    /** 
+    * Show the form for creating a new resource. 
+    * @return \Illuminate\Http\Response 
+    
+    */
+    public function create() {
+        $j = Ampayer::orderBy('idAmpayer', 'DESC')->first();
+        $lastId = $j->idAmpayer;
+        return response(view('Apayers.create', compact('lastId')));
+    }
+    /** 
+     * Store a newly created resource in storage. 
+     * 
+     * @param  \Illuminate\Http\Request   $request 
+     * @return \Illuminate\Http\Response 
+     */
+    public function store(Request $request) {
+
         $a = new Ampayer();
         $a -> nombre = $request -> nombre;
         $a ->apellidos = $request -> apellidos;
@@ -15,6 +42,55 @@ class AmpayerControlador extends Controller
         $a -> abreviacion = $request -> abreviacion;
         $a -> activo = $request -> activo;
         $a -> save();
-        
+        return response()->redirectTo(route("ampayersIndex"))
+            ->with(["success" => "Actulizado exitosamente"])
+            ->header('Cache-Control', 'no-store, no-cache, must-revalidate');
+
+    }
+    /** 
+     * Display the specified resource. 
+     * @param  int  $id 
+     * @return \Illuminate\Http\Response 
+     */
+    public function show($id) {
+
+        //  
+    }
+    /** 
+     * Show the form for editing the specified resource. 
+     * @param  int  $id 
+     * @return  \Illuminate\Http\Response 
+     */
+    public function edit($id) {
+        $datos = Ampayer::find($id);
+        return response(view("Ampayers.edit", compact("datos")));
+    }
+    /** 
+     * Update the specified resource in storage. 
+     * @param  \Illuminate\Http\Request   $request 
+     * @param  int  $id 
+     * @return \Illuminate\Http\Response 
+     */
+    public function update(Request $request) {
+        $a = Ampayer::find($request->idAmpayer);
+        $a -> nombre = $request -> nombre;
+        $a ->apellidos = $request -> apellidos;
+        $a -> fechaNacimiento = $request -> fechaNacimiento;
+        $a -> curp = $request -> curp;
+        $a -> abreviacion = $request -> abreviacion;
+        $a -> activo = $request -> activo;
+        $a -> save();
+        return response() ->redirectTo(route("ampayersIndex"))
+        ->with(["success" => "Actulizado exitosamente"])
+        ->header('Cache-Control', 'no-store, no-cache, must-revalidate');
+
+    }
+    /** 
+     * Remove the specified resource from storage. 
+     * @param  int  $id 
+     * @return  \Illuminate\Http\Response 
+     */
+    public function destroy($id) {
+
     }
 }
--- app/Http/Controllers/JuegoControlador.php
+++ app/Http/Controllers/JuegoControlador.php
@@ -5,21 +5,103 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Juego;
 
-class JuegoControlador extends Controller
-{
-    public function create(Request $request){
+class JuegoControlador extends Controller {
+
+
+    /** 
+     * Display a listing of the resource. 
+     * @return \Illuminate\Http\Response 
+     */
+    public function index() {
+
+        $datos = Juego::all();
+        return response(view("Juegos.index", compact("datos")));
+    }
+    /** 
+    * Show the form for creating a new resource. 
+    * @return \Illuminate\Http\Response 
+    
+    */
+    public function create() {
+        $j = Juego::orderBy('idJuego', 'DESC')->first();
+        $lastId = $j->idJuego;
+        return response(view('Juegos.create', compact('lastId')));
+    }
+    /** 
+     * Store a newly created resource in storage. 
+     * 
+     * @param  \Illuminate\Http\Request   $request 
+     * @return \Illuminate\Http\Response 
+     */
+    public function store(Request $request) {
+
         $j = new Juego();
-        $j->idTemporada = $request -> idTemporada;
-        $j -> jornada = $request -> jornada;
-        $j -> idCampo = $request -> idCampo;
-        $j -> fecha = $request -> fecha;
-        $j -> hora = $request -> hora;
-        $j -> clima = $request -> clima;
-        $j -> idEquipoVisitante = $request -> idEquipoVisitante;
-        $j -> idEquipoLocal = $request -> idEquipoLocal;
-        $j -> final = $request -> final;
-        $j -> idAviso = $request -> idAviso;
-        $j -> idCuerpo = $request -> idCuerpo;
-        $j -> save();
+        $j->idTemporada = $request->idTemporada;
+        $j->jornada = $request->jornada;
+        $j->idCampo = $request->idCampo;
+        $j->fecha = $request->fecha;
+        $j->hora = $request->hora;
+        $j->clima = $request->clima;
+        $j->idEquipoVisitante = $request->idEquipoVisitante;
+        $j->idEquipoLocal = $request->idEquipoLocal;
+        $j->final = $request->final;
+        $j->idAviso = $request->idAviso;
+        $j->idCuerpo = $request->idCuerpo;
+        $j->save();
+        return response()->redirectTo(route("juegosIndex"))
+            ->with(["success" => "Actulizado exitosamente"])
+            ->header('Cache-Control', 'no-store, no-cache, must-revalidate');
+
+    }
+    /** 
+     * Display the specified resource. 
+     * @param  int  $id 
+     * @return \Illuminate\Http\Response 
+     */
+    public function show($id) {
+
+        //  
+    }
+    /** 
+     * Show the form for editing the specified resource. 
+     * @param  int  $id 
+     * @return  \Illuminate\Http\Response 
+     */
+    public function edit($id) {
+        $datos = Juego::find($id);
+        return response(view("Juegos.edit", compact("datos")));
+    }
+    /** 
+     * Update the specified resource in storage. 
+     * @param  \Illuminate\Http\Request   $request 
+     * @param  int  $id 
+     * @return \Illuminate\Http\Response 
+     */
+    public function update(Request $request) {
+        $j = Juego::find($request->idJuego);
+        $j->idTemporada = $request->idTemporada;
+        $j->jornada = $request->jornada;
+        $j->idCampo = $request->idCampo;
+        $j->fecha = $request->fecha;
+        $j->hora = $request->hora;
+        $j->clima = $request->clima;
+        $j->idEquipoVisitante = $request->idEquipoVisitante;
+        $j->idEquipoLocal = $request->idEquipoLocal;
+        $j->final = $request->final;
+        $j->idAviso = $request->idAviso;
+        $j->idCuerpo = $request->idCuerpo;
+        $j->save();
+        return response() ->redirectTo(route("juegosIndex"))
+        ->with(["success" => "Actulizado exitosamente"])
+        ->header('Cache-Control', 'no-store, no-cache, must-revalidate');
+
+    }
+    /** 
+     * Remove the specified resource from storage. 
+     * @param  int  $id 
+     * @return  \Illuminate\Http\Response 
+     */
+    public function destroy($id) {
+
     }
 }
