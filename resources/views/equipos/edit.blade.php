3c6c0bcec3c12ff93c6ede46965348dfc5c6446b
Cruzdelangelrubiofierro <125680938+Cruzdelangelrubiofierro@users.noreply.github.com>
2023-03-15 07:23:01 -0700

    r

--- resources/views/equipos/edit.blade.php
+++ resources/views/equipos/edit.blade.php
@@ -22,39 +22,24 @@
                             value={{ $datos->nombre }}>
                     </div>
                     <div class="col">
-                        <label for="apellidos" class="form-label">Apellidos:</label>
-                        <input type="text" name="apellidos" class="form-control" id="apellidos" required
-                            value={{ $datos->apellidos }}>
+                        <label for="idLogo" class="form-label">IdLogo:</label>
+                        <input type="text" name="idLogo" class="form-control" id="idLogo" required
+                            value={{ $datos->idLogo }}>
                     </div>
                     <div class="col">
-                        <label for="abreviacion" class="form-label">Abreviación:</label>
-                        <input type="text" name="abreviacion" class="form-control" id="abreviacion"
-                            value={{ $datos->abreviacion }}>
+                        <label for="ciudad" class="form-label">Ciudad:</label>
+                        <input type="text" name="ciudad" class="form-control" id="ciudad"
+                            value={{ $datos->ciudad }}>
                     </div>
                 </div>
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="fechaNacimiento" class="form-label">Fecha de Nacimiento:</label>
-                        <input type="date" name="fechaNacimiento" class="form-control" id="fechaNacimiento" required
-                            value={{ $datos->fechaNacimiento }}>
-                    </div>
-                    <div class="col">
-                        <label for="curp" class="form-label">CURP:</label>
-                        <input type="text" name="curp" class="form-control" id="curp"   
-                            value={{ $datos->curp }}>
+                        <label for="idTecnico" class="form-label">idTecnico:</label>
+                        <input type="text" name="idTecnico" class="form-control" id="idTecnico"   
+                            value={{ $datos->idTecnico }}>
                     </div>
                 </div>
-                <div class="row mb-3">
-                    <div class="col">
-                        <label for="status" class="form-label">Estatus:</label>
-                        <select name="status" id="status" class="form-select" required value={{ $datos->status }}>
-                            <option value="">Seleccione una opción</option>
-                            <option value="1">Activo</option>
-                            <option value="0">Inactivo</option>
-                        </select>
-                    </div>
-
-                </div>
+             
                 <div class="row">
                     <div class="col justify-content-center">
                         <button type="submit" class="btn btn-primary w-100">guardar</button>
