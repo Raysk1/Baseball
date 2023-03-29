779ff9fcb2c5805544b8331dc94d43fe82e9a512
Evaristo Calderon <51042100+Evaristo07@users.noreply.github.com>
2023-03-29 09:38:43 -0700

    Se agrego la View de Lanzadores con las pantalla

--- resources/views/AmpayersJuegos/create.blade.php
+++ resources/views/AmpayersJuegos/create.blade.php
@@ -7,47 +7,51 @@
     <form action={{ route('AmpayersJuegoStore') }} method="post">
         @csrf
         <div class="d-flex justify-content-center">
-            <form action = {{'AmpayersJuegoUpdate'}} method = "POST">
+            <div class="w-75 mt-4">
 
-            <div class="row mb-3">
-                <div class="row">
-                    <div class="col">
-                        <h1 class="text-center">AMPAYERS JUEGO</h1><br>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <label for="idJuego" class="form-label">ID Juego:</label>
-                        <input type="text" class="form-control" name="idJuego" id="idJuego" required @readonly(true)>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <label for="idAmpayer" class="form-label">ID Ampayer:</label>
-                        <input type="text" name="idAmpayer" class="form-control" id="idAmpayer" required @readonly(true)>
+                <form action={{ 'AmpayersJuegoUpdate' }} method="POST">
+
+                    <div class="row mb-3">
+                        <div class="row">
+                            <div class="col">
+                                <h1 class="text-center">AMPAYERS JUEGO</h1><br>
+                            </div>
                         </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <label for="idCuerpo" class="form-label">ID Cuerpo:</label>
-                        <input type="text" name="idCuerpo" class="form-control" id="idCuerpo" required @readonly(true)>
+                        <div class="row">
+                            <div class="col">
+                                <label for="idJuego" class="form-label">ID Juego:</label>
+                                <input type="text" class="form-control" name="idJuego" id="idJuego" required
+                                    @readonly(true)>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <label for="idAmpayer" class="form-label">ID Ampayer:</label>
+                                <input type="text" name="idAmpayer" class="form-control" id="idAmpayer" required
+                                    @readonly(true)>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <label for="idCuerpo" class="form-label">ID Cuerpo:</label>
+                                <input type="text" name="idCuerpo" class="form-control" id="idCuerpo" required
+                                    @readonly(true)>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <label for="ubicacion" class="form-label">Ubicacion:</label>
+                                <input type="text" name="ubicacion" class="form-control" id="ubiacion" required>
 
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col justify-content-center"><br><br>
+                                <button type="submit" class="btn btn-primary w-100">GUARDAR</button>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <label for="ubicacion" class="form-label">Ubicacion:</label>
-                        <input type="text" name="ubicacion" class="form-control" id="ubiacion" required>
-
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col justify-content-center"><br><br>
-                        <button type="submit" class="btn btn-primary w-100">GUARDAR</button>
-                    </div>
-                </div>
-            </form>
-
+                </form>
             </div>
         </div>
     </form>
