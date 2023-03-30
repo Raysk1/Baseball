3637595363e2608777f572feff5554ef99e157e3
MiniBarraza <125677090+MiniBarraza@users.noreply.github.com>
2023-03-29 23:09:07 -0700

    edite pagina de incio

--- resources/views/inicio/inicio.blade.php
+++ resources/views/inicio/inicio.blade.php
@@ -13,15 +13,11 @@
 
     <div class="contenedor">
 
-        <body background="/img/basebal.jpg">
+        <body background="/img/basebale.jpg">
     </div>
     </div>
     <div class="caja1" img src="/img/fondo.jpg" width="30%" height="30%">
-        <img src="/img/images.jpg" width="5%" height="5%" class="img-responsive">
-        <img src="/img/caco.jpg" width="10%" height="10%" class="img-responsive">
-        <img src="/img/bat.jpg" width="5%" height="5%" class="img-responsive">
-        <img src="/img/guante.jpg" width="10%" height="10%" class="img-responsive">
-        <img src="/img/pelota.jpg" width="10%" height="10%" class="img-responsive">
+        
     </div>
     <img src="/img/imagenlogo.jpg" width="50%" height="50%">
 
