3bf124be6c479109a65e13ce8e073be9dd80ffaa
MiniBarraza <125677090+MiniBarraza@users.noreply.github.com>
2023-03-30 07:19:35 -0700

    edite pagina de inicio

--- resources/views/inicio/inicio.blade.php
+++ resources/views/inicio/inicio.blade.php
@@ -9,17 +9,16 @@
 </head>
 
 <body>
-    <h1><mark> JUEGOS DE BASEBALL 2023</mark></h1>
 
     <div class="contenedor">
 
-        <body background="/img/basebale.jpg">
+        <body background="/img/baseball (3).jpg">
     </div>
     </div>
-    <div class="caja1" img src="/img/fondo.jpg" width="30%" height="30%">
-        
+    <div class="caja1" img src="/img/.jpg" width="30%" height="30%">
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> 
     </div>
-    <img src="/img/imagenlogo.jpg" width="50%" height="50%">
+    
 
 </body>
 
