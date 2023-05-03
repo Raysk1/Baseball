9ea169997c6af8558c24b90bd9e4df2af6a9942a
Evaristo Calderon <51042100+Evaristo07@users.noreply.github.com>
2023-05-03 07:24:46 -0700

    Correcciones

--- resources/views/sesion/login.blade.php
+++ resources/views/sesion/login.blade.php
@@ -33,35 +33,6 @@
             placeholder="Ingrese su contraseña" required>
         <button type="submit" class="btn btn-danger btn-flat"> Iniciar Sesion </button>
         <p class="link"><a href="/registrologin">¿Aun no tienes cuenta?</a></p>
-
-
-
-        <script>
-            function validarUsuario() {
-                // Obtener los valores de los campos de usuario y contraseña
-                var email = document.getElementById("email").value;
-                var password = document.getElementById("password").value;
-
-                // Validar que los campos no estén vacíos
-                if (email == "" || password == "") {
-                    alert("Por favor, ingrese usuario y contraseña.");
-                    return false;
-                }
-
-                // Validar que el usuario y la contraseña sean correctos
-                // Aquí podrías hacer una petición AJAX al servidor para validar los datos
-                if (email != "email" || password != "contraseña") {
-                    alert("Usuario o contraseña incorrectos.");
-                    return false;
-                }
-
-                // Si los datos son válidos, redirigir al usuario a la página de inicio
-                return view("/inicio.inicio");
-            }
-        </script>
-
-
-
     </form>
 </body>
 
