b8acc6920a5e61d05e877b2068e6c2614fba4853
DELLAlfredo <119427135+DELLAlfredo@users.noreply.github.com>
2023-05-08 07:34:10 -0700

    ay

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -51,7 +51,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href={{ route('Noticias') }}><b> Inicio
+                            <a class="nav-link active" aria-current="page" href={{ route('inicio') }}><b> Inicio
                                 </b></a>
                         </li>
                         <li class="nav-item dropdown">
