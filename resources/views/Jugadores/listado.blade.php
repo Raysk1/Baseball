c0d6e07a7429cf03e67d180100e1cb4d7dffff39
Evaristo Calderon <51042100+Evaristo07@users.noreply.github.com>
2023-05-09 10:13:44 -0700

    Tarjetas

--- resources/views/Jugadores/listado.blade.php
+++ resources/views/Jugadores/listado.blade.php
@@ -6,49 +6,26 @@
 @section('content')
     <div class="d-flex justify-content-center">
         <div class="w-75 mt-4">
-            <h1>Prueba</h1>
-
-            <table border="1px">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nombre</th>
-                    </tr>
-                </thead>
-                <tbody>
-
-                    @foreach ($jugadores as $jugador)
-                        <tr>
-                            <td>{{ $jugador->idAfiliacion }}</td>
-                            <td>{{ $jugador->nombre }}</td>
-                            <td>{{ $jugador->apellidos }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-
-
-
             <div class="container">
                 <div class="row justify-content-between">
 
-
                     @foreach ($jugadores as $jugador)
-                    <div class="card" style="width: 18rem;">
-                        <img src="../../../public/img/jugadores/230000.png" class="card-img-top" alt="...">
-                        <div class="card-body"><br>
-                            <img src="" alt="">
-                            ID: {{$jugador->idAfiliacion}}
-                            {{$jugador->nombre}} {{$jugador->apellidos}}
-
-
+                        <div class="card" style="width: 18rem;">
+                            <img src="/img/jugadores/{{ $jugador->idAfiliacion }}.png" class="card-img-top img-fluid mt-2" alt="/img/jugadores/chicamala.jpg" style="max-width: 100%; height: 60%;">
+                            <div class="card-body p-1">
+                                ID: {{ $jugador->idAfiliacion }}<br>
+                                <b>{{ $jugador->nombre }} {{ $jugador->apellidos }}<br></b>
+                                Rama: {{ $jugador->rama }}<br>
+                                Posicion: {{ $jugador->posicion }}<br>
+                                <b>Golpea: </b>{{ $jugador->golpea }} 
+                                <b>Tira: </b>{{ $jugador->tira }}<br>
+                                Ab: {{ $jugador->abreviacion }}
+                            </div>
                         </div>
-                    </div>
-
                     @endforeach
-                    
-
 
                 </div>
             </div>
-            @endsection
+        </div>
+    </div>
+    @endsection
