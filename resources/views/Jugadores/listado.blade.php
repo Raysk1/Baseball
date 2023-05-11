fe6c8060a82e476b8df6baf0f8fed5ea8581f730
Evaristo Calderon <51042100+Evaristo07@users.noreply.github.com>
2023-05-11 08:37:21 -0700

    Validacion de imagen para los jugadores

--- resources/views/Jugadores/listado.blade.php
+++ resources/views/Jugadores/listado.blade.php
@@ -12,18 +12,25 @@
 
                     @foreach ($jugadores as $jugador)
                         <div class="card" style="width: 18rem;">
-                            <img src="/img/jugadores/{{ $jugador->idAfiliacion }}.png" class="card-img-top img-fluid mt-2"
-                                alt="{{$jugador->genero == 'Varonil' ? '/img/jugadores/Varonil.jpg' : '/img/jugadores/Femenil.jpg'}}" style="max-width: 100%; height: 60%;">
+                            <!--Validar imagen para que en caso que no exista la imagen del jugador se muestre una imagen por defecto si es varonil o femenil-->
+                            @if (file_exists('/img/jugadores/' . $jugador->idAfiliacion . '.png'))
+                                <img src="/img/jugadores/{{ $jugador->idAfiliacion }}.png" class="card-img-top img-fluid mt-2"
+                                    style="max-width: 100%; height: 60%;">
+                            @else
+                                <img src="{{ $jugador->rama == 'Varonil' ? '/img/jugadores/Varonil.jpg' : '/img/jugadores/Femenil.jpg' }}"
+                                    class="card-img-top img-fluid mt-2" style="max-width: 100%; height: 60%;">
+                            @endif
+
                             <div class="card-body p-1">
                                 ID: {{ $jugador->idAfiliacion }}<br>
-                                <b>{{ $jugador->nombre }} {{ $jugador->apellidos }}<br></b>
-                                <b>Ab: </b>{{ $jugador->abreviacion }} 
+                                <b>{{ $jugador->nombre }} {{ $jugador->apellidos }}<br></b><br>
+                                <b>Ab: </b>{{ $jugador->abreviacion }}
                                 <b>Rama: </b>{{ $jugador->rama }}<br>
                                 <b>Posicion: </b>{{ $jugador->posicion }}<br>
                                 <b>Golpea: </b>{{ $jugador->golpea }}
-                                <b>Tira: </b> {{$jugador->tira }}<br>
+                                <b>Tira: </b> {{ $jugador->tira }}<br>
 
-                                
+
                             </div>
                         </div>
                     @endforeach
