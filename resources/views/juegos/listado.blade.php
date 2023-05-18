de471b5813b89a3b1c177fb88c41d172ec76c9d6
Evaristo Calderon <51042100+Evaristo07@users.noreply.github.com>
2023-05-18 08:03:21 -0700

    rutas del listado de Juego

---
+++ resources/views/juegos/listado.blade.php
@@ -0,0 +1,66 @@
+@extends('default')
+@section('title')
+    Listado de Juegos
+@endsection
+
+@section('content')
+    <div class="d-flex justify-content-center">
+        <div class="w-75 mt-4">
+            <h1>Listado de Juegos</h1>
+            <div class="container">
+                <div class="row justify-content-between">
+
+
+
+                    <div class="table-responsive">
+                        <table class="table table-responsive table-sm table-dark table-striped table-bordered table-hover">
+                            <tr>
+                                <th>Celda 1</th>
+                                <th>Celda 2</th>
+                                <th>Celda 3</th>
+                                <th>Celda 4</th>
+                                <th>Celda 5</th>
+                            </tr>
+                            <tr>
+                                <td>Celda 1</td>
+                                <td>Celda 2</td>
+                                <td>Celda 3</td>
+                                <td>Celda 4</td>
+                                <td>Celda 5</td>
+                            </tr>
+                            <tr>
+                                <td>Celda 1</td>
+                                <td>Celda 2</td>
+                                <td>Celda 3</td>
+                                <td>Celda 4</td>
+                                <td>Celda 5</td>
+                            </tr>		
+                            <tr>
+                                <td>Celda 1</td>
+                                <td>Celda 2</td>
+                                <td>Celda 3</td>
+                                <td>Celda 4</td>
+                                <td>Celda 5</td>
+                            </tr>
+                            <tr>
+                                <td>Celda 1</td>
+                                <td>Celda 2</td>
+                                <td>Celda 3</td>
+                                <td>Celda 4</td>
+                                <td>Celda 5</td>
+                            </tr>		
+                        </table>
+                        </div>
+
+
+
+
+
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
