245b6882fcdeaf3ac5e25435f95ff68c30486a21
Evaristo Calderon <51042100+Evaristo07@users.noreply.github.com>
2023-05-20 16:51:14 -0700

    listado

--- resources/views/juegos/listado.blade.php
+++ resources/views/juegos/listado.blade.php
@@ -2,6 +2,7 @@
 @section('title')
     Listado de Juegos
 @endsection
+
 @section('content')
     <div class="d-flex justify-content-center">
         <div class="w-75 mt-4">
@@ -9,43 +10,45 @@
             <div class="container">
                 <div class="row justify-content-between">
 
-                    <div class="card" style="width: 33%;">
+                    @foreach ($juegos as $j)
+                        <div class="card" style="width: 33%;">
+                            <div class="table-responsive">
 
-                        <table class="table table-responsive table-hover">
-                            <tr>
-                                <th colspan="5">FINAL</th>
-                            </tr>
-                            <tr>
-                                <th colspan="2"></th>
-                                <th>C</th>
-                                <th>H</th>
-                                <th>E</th>
-                            </tr>
+                                <table class="table table-responsive table-hover">
+                                    <tr>
+                                        <th colspan="5">FINAL</th>
+                                    </tr>
+                                    <tr>
+                                        <th></th>
+                                        <th></th>
+                                        <th>C</th>
+                                        <th>H</th>
+                                        <th>E</th>
+                                    </tr>
 
-                            <tr>
-                                <td><img src="/img/juegos/1.png"></td>
-                                <td>Local</td>
-                                @foreach ($juegos as $j)
-                                    <td>{{ $j->carrerasLocal }}</td>
-                                    <td>{{ $j->hitsLocal }}</td>
-                                    <td>{{ $j->erroresLocal }}</td>
-                                @endforeach
-                            </tr>
+                                    <tr>
+                                        <td><img src="/img/juegos/1.png"></td>
+                                        <td>Local</td>
+                                        <td>{{ $j->carrerasLocal }}</td>
+                                        <td>{{ $j->hitsLocal }}</td>
+                                        <td>{{ $j->erroresLocal }}</td>
+                                    </tr>
 
-                            <tr>
-                                <td><img src="/img/juegos/1.png"></td>
-                                <td>Visitante</td>
-                                @foreach ($juegos as $j)
-                                    <td>{{ $j->carrerasVisitante }}</td>
-                                    <td>{{ $j->hitsVisitante }}</td>
-                                    <td>{{ $j->erroresVisitante }}</td>
-                                @endforeach
-                            </tr>
-                        </table>
-                    </div>
+                                    <tr>
+                                        <td><img src="/img/juegos/1.png"></td>
+                                        <td>Visitante</td>
+                                        <td>{{ $j->carrerasVisitante }}</td>
+                                        <td>{{ $j->hitsVisitante }}</td>
+                                        <td>{{ $j->erroresVisitante }}</td>
+                                    </tr>
+                                </table>
+
+                            </div>
+
+                        </div>
+                    @endforeach
 
                 </div>
             </div>
         </div>
-    </div>
-@endsection
+    @endsection
