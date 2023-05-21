c2db54f6efb26ba4ce264dfaa3bcd2a19ba1a7b1
Evaristo Calderon <51042100+Evaristo07@users.noreply.github.com>
2023-05-20 22:51:54 -0700

    Listado de Juegos//100

--- app/Http/Controllers/BateadorControlador.php
+++ app/Http/Controllers/BateadorControlador.php
@@ -3,15 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Bateador;
-use App\Models\Equipo;
 use App\Models\Juego;
 use Illuminate\Http\Request;
 
-class BateadorControlador extends Controller {
+class BateadorControlador extends Controller
+{
     /**
      * Display a listing of the resource.
      */
-    public function index() {
+    public function index()
+    {
         $datos = Bateador::all();
         return response(view("Bateadores.index", compact("datos")));
     }
@@ -19,7 +20,8 @@ class BateadorControlador extends Controller {
     /**
      * Show the form for creating a new resource.
      */
-    public function create($idJuego) {
+    public function create($idJuego)
+    {
         $t = Bateador::orderBy('idBateadores', 'DESC')->first();
         $lastId = $t == null ? 1 : $t->idBateadores + 1;
         $j = Juego::find($idJuego);
@@ -32,7 +34,8 @@ class BateadorControlador extends Controller {
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $t = new Bateador();
         $t->idJuego = $request->idJuego;
         $t->AB = $request->AB;
@@ -41,26 +44,32 @@ class BateadorControlador extends Controller {
         $t->CP = $request->CP;
         $t->BB = $request->BB;
         $t->K = $request->K;
-        $t->PJE = $request->PJE;
-        $t->OBP = $request->OBP;
-        $t->idBateadores = $request->idBateadores;
+
+        $t->PJE = ($t->AB != 0) ? number_format($t->H / $t->AB, 3) : 0; // Calcula el promedio de bateo
+        $total_bases = $t->H + $t->BB;
+        $turnos_plato = $t->AB + $t->BB;
+        $t->OBP = ($turnos_plato != 0) ? number_format($total_bases / $turnos_plato, 3) : 0;
+
+        $t->idAfiliacion = $request->idAfiliacion;
         $t->save();
-        return response()->redirectTo(route("juegosDetails", ["juegoId" => $t->idJuego]))
+        return response()->redirectTo(route("juegosDetails", ["id" => $t->idJuego]))
             ->with(["success" => "Actulizado exitosamente"])
             ->header('Cache-Control', 'no-store, no-cache, must-revalidate');
     }
-
+0
     /**
      * Display the specified resource.
      */
-    public function show(string $id) {
+    public function show(string $id)
+    {
         //
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit($id) {
+    public function edit($id)
+    {
         $b = Bateador::find($id);
         $j = $b->juego;
         $jugadores = $j->equipoVisitante->jugadores;
@@ -72,22 +81,21 @@ class BateadorControlador extends Controller {
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request) {
+    public function update(Request $request)
+    {
         $t = Bateador::find($request->idBateadores);
         $t->idJuego = $request->idJuego;
         $t->idBateadores = $request->idBateadores;
         $t->AB = $request->AB;
-        $t->C = $request ->C;
-        $t->H = $request -> H;
-        $t->CP = $request -> CP;
-        $t->BB = $request -> BB;
-        $t->K = $request -> K;
-        $t->PJE = ($t->AB != 0) ? number_format($t->H / $t->AB, 3)  : 0; // Calcula el promedio de bateo
+        $t->C = $request->C;
+        $t->H = $request->H;
+        $t->CP = $request->CP;
+        $t->BB = $request->BB;
+        $t->K = $request->K;
+        $t->PJE = ($t->AB != 0) ? number_format($t->H / $t->AB, 3) : 0; // Calcula el promedio de bateo
         $total_bases = $t->H + $t->BB;
         $turnos_plato = $t->AB + $t->BB;
-        $t->OBP = ($turnos_plato != 0) ?number_format( $total_bases / $turnos_plato,3) : 0;
-
-
+        $t->OBP = ($turnos_plato != 0) ? number_format($total_bases / $turnos_plato, 3) : 0;
 
         $t->save();
         return response()->redirectTo(route("juegosDetails", ["id" => $t->idJuego]))
@@ -98,7 +106,8 @@ class BateadorControlador extends Controller {
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id) {
+    public function destroy(string $id)
+    {
         //
     }
 }
