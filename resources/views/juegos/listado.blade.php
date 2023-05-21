bd65da880c8dda9cf4cb63bbdf79cd25e175cedf
Evaristo Calderon <51042100+Evaristo07@users.noreply.github.com>
2023-05-20 23:43:17 -0700

    listado

--- resources/views/juegos/listado.blade.php
+++ resources/views/juegos/listado.blade.php
@@ -19,38 +19,38 @@
                                         $rutaImagenL = '/img/equipos/' . $j->idEquipoLocal . '.png';
                                         $rutaImagenV = '/img/equipos/' . $j->idEquipoVisitante . '.png';
                                     @endphp
+
                                     <tr>
                                         <th colspan="5">FINAL</th>
                                     </tr>
                                     <tr>
-                                        <th></th>
-                                        <th></th>
+                                        <th colspan="2"></th>
                                         <th>C</th>
                                         <th>H</th>
                                         <th>E</th>
                                     </tr>
 
                                     <tr>
-                                        <td><img src="{{ asset($rutaImagenL)}}" class="img-fluid rounded"
+                                        <td><img src="{{ asset($rutaImagenL) }}" class="img-fluid rounded"
                                                 style="width: 25px; height: 25px;"></td>
-                                        <td>Local: {{ $j -> equipoLocal -> nombre}}</td>
+                                        <td>Local: {{ $j->equipoLocal->nombre }}</td>
                                         <td>{{ $j->carrerasLocal }}</td>
                                         <td>{{ $j->hitsLocal }}</td>
                                         <td>{{ $j->erroresLocal }}</td>
                                     </tr>
 
                                     <tr>
-                                        <td><img src="{{ asset($rutaImagenV)}}" class="img-fluid rounded"
-                                            style="width: 25px; height: 25px;"></td>
-                                            <td>Visitante: {{ $j -> equipoVisitante -> nombre}}</td>
+                                        <td><img src="{{ asset($rutaImagenV) }}" class="img-fluid rounded"
+                                                style="width: 25px; height: 25px;"></td>
+                                        <td>Visitante: {{ $j->equipoVisitante->nombre }}</td>
                                         <td>{{ $j->carrerasVisitante }}</td>
                                         <td>{{ $j->hitsVisitante }}</td>
                                         <td>{{ $j->erroresVisitante }}</td>
                                     </tr>
                                 </table>
-                                <a href="{{ route("boxscore", ["id"=>$j->idJuego])}} "  class="btn btn-primary w-100 mb-3">Boxscore</a>
+                                <a href="{{ route('boxscore', ['id' => $j->idJuego]) }} "
+                                    class="btn btn-primary w-100 mb-3">Boxscore</a>
                             </div>
-
                         </div>
                     @endforeach
 
